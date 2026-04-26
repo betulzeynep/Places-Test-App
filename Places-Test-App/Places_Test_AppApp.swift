e6d569e411151ad5621b193e53ce9b6252c2dfaa
LogicAndLines <betulzeynepseyis@gmail.com>
2026-04-26 15:07:15 +0200

    Initial Commit

---
+++ Places-Test-App/Places_Test_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Places_Test_AppApp.swift
+//  Places-Test-App
+//
+//  Created by Zeynep Turnalı on 26.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct Places_Test_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
